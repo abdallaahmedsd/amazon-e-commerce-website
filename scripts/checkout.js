2e119fe2469a613cb9fad5fd57083c56411508b6
Abdalla <abdallaahmedsudanese@gmail.com>
2024-08-03 15:02:50 +0300

    save the cart into local storage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,7 +20,7 @@ function generateAndShowCartItems() {
       throw Error(`There's no product with Id = ${productId}`);
 
     cartSummaryHTML += `
-      <div class="cart-item-container">
+      <div class="cart-item-container js-cart-item-container-${product.id}">
         <div class="delivery-date">
           Delivery date: Tuesday, June 21
         </div>
@@ -112,7 +112,13 @@ function removeFromCart() {
   deleteLinks.forEach(link => {
     link.addEventListener('click', (e) => {
       let {productId} = e.target.dataset;
-      cartModule.removeFromCart(productId);
+
+      if(cartModule.removeFromCart(productId)) {
+        let container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+        container.remove();
+      }
+
     });
   });
 }
